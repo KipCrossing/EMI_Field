c77f484206608ec2d3ac0fe709e4c918c090dff5
KipCrossing <kip.crossing@gmail.com>
2019-09-09 15:40:27 +1000

    Allign 10 am run

---
+++ Cobbity9/combine.py
@@ -0,0 +1,31 @@
+# this codd combinds the GPS data with the OpenEM data
+import pandas as pd
+
+gps_df = pd.read_csv('CobbityGPS_2pm.csv')
+openem_df = pd.read_csv('Cobbity9_run2.csv')
+print(list(openem_df))
+output_df = gps_df
+
+# ID,Amp,Shift,Shift_out,Voltage,Temp,Humidity,Hs,Hp
+output_df['Amp'] = output_df['ID']
+output_df['Shift'] = output_df['ID']
+output_df['Voltage'] = output_df['ID']
+output_df['Temp'] = output_df['ID']
+output_df['Humidity'] = output_df['ID']
+output_df['Hs'] = output_df['ID']
+output_df['Hp'] = output_df['ID']
+
+print(output_df.head())
+
+for id_i in range(len(gps_df['ID'])):
+    for id_j in range(len(openem_df['ID'])):
+        if gps_df['ID'][id_i] == openem_df['ID'][id_j]:
+            output_df['Amp'][id_i] = openem_df['Amp_rolling'][id_j]
+            output_df['Shift'][id_i] = openem_df['Shift'][id_j]
+            output_df['Voltage'][id_i] = openem_df['Volt_rolling'][id_j]
+            output_df['Temp'][id_i] = openem_df['Temp_rolling'][id_j]
+            output_df['Humidity'][id_i] = openem_df['Humidity'][id_j]
+            output_df['Hs'][id_i] = openem_df['Hs_rolling'][id_j]
+            output_df['Hp'][id_i] = openem_df['Hp_rolling'][id_j]
+
+output_df.to_csv('OpenEM_Cobbity_2pm_all.csv', index=False)
